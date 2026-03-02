888cbda7971fc112bc9e24be37b289abf346c4ef
Xeltalliv <vadim@vvv.kiev.ua>
2026-03-02 05:20:11 +0200

    Add disclaimer

--- client/mainInstance.mjs
+++ client/mainInstance.mjs
@@ -1,5 +1,7 @@
 import Main from "./Main/Main.mjs";
 
+alert("GoVed/Shurikun's 'Elemental' game for Android was about combining elements to unlock new elements, with the catch that all elements other than the first 4 were suggested by the players and automatically added into the game upon reaching enough votes. The original game shut down and is no longer playable. This is a static offline preservation project of data from it without any accounts or voting systems.");
+
 const main = new Main();
 
 window.main = main;
